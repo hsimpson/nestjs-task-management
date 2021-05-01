0e1d894eb7ea55c251dbaaf4e8e51bba022d7f3c
Daniel Toplak <homer.simpson123@gmx.de>
2021-05-01 19:42:27 +0200

    feat: implement JWT token handling

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    '@typescript-eslint/require-await': 'error',
   },
 };
